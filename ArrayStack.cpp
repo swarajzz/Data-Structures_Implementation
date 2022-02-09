00eb834a2ddac7ae1f94bed5df5c070a395c7391
swarajzz <sharma.swaraj0403@gmail.com>
2022-02-09 08:09:17 +0530

    Added Comments

--- ArrayStack.cpp
+++ ArrayStack.cpp
@@ -17,16 +17,19 @@ public:
         capacity = totalSize;
     }
 
+    //Total number of elements in the stack
     int size()
     {
         return nextIndex;
     }
 
+    //Check whether stack is empty or not
     bool isEmpty()
     {
         return nextIndex == 0;
     }
 
+    //Insert into stack
     void push(int element)
     {
         if (capacity == nextIndex)
@@ -39,6 +42,7 @@ public:
         nextIndex++;
     }
 
+    // Delete and return the top element
     int pop()
     {
         if (isEmpty())
@@ -50,6 +54,7 @@ public:
         return data[nextIndex];
     }
 
+    //Return the Top element in the stack
     int top()
     {
         if (isEmpty())
@@ -60,29 +65,3 @@ public:
         return data[nextIndex - 1];
     }
 };
-
-int main()
-{
-    // cout << "Hello" << endl;
-    StackusingArray s(4);
-
-    s.push(10);
-    s.push(20);
-    s.push(30);
-    s.push(40);
-    s.push(50);
-
-    // cout << s.top() << endl;
-    // cout << s.pop() << endl;
-    // cout << s.pop() << endl;
-    // cout << s.pop() << endl;
-
-    // cout << s.size() << endl;
-
-    // cout << s.isEmpty() << endl;
-
-    while (!s.isEmpty())
-    {
-    cout << s.pop() << endl;
-    }
-}
