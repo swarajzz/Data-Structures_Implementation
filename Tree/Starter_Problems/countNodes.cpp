176ef625f9f2046edc9112d98a0fe7786dcf64bb
swarajzz <sharma.swaraj0403@gmail.com>
2022-02-20 21:05:47 +0530

    total nodes

--- Tree/Starter_Problems/countNodes.cpp
+++ Tree/Starter_Problems/countNodes.cpp
@@ -1,38 +1,3 @@
-#include <iostream>
-#include "../TreeNode.h"
-#include <climits>
-#include <queue>
-
-TreeNode<int> *takeInputLevelWise()
-{
-    int rootData;
-    cout << "Enter root's data" << endl;
-    cin >> rootData;
-
-    TreeNode<int> *root = new TreeNode<int>(rootData);
-
-    queue<TreeNode<int> *> pendingNodes;
-    pendingNodes.push(root);
-    while (pendingNodes.size() != 0)
-    {
-        TreeNode<int> *front = pendingNodes.front();
-        pendingNodes.pop();
-        cout << "Enter num of children of " << front->data << endl;
-        int numChild;
-        cin >> numChild;
-        for (int i = 0; i < numChild; i++)
-        {
-            int childData;
-            cout << "Enter " << i << "th child of " << front->data << endl;
-            cin >> childData;
-            TreeNode<int> *child = new TreeNode<int>(childData);
-            front->children.push_back(child);
-            pendingNodes.push(child);
-        }
-    }
-    return root;
-}
-
 int countNodes(TreeNode<int> *root)
 {
     int count = 1;
