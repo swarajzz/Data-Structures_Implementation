b2d06a542fb933e5dc8909a1347b6262fc57d2ce
swarajzz <sharma.swaraj0403@gmail.com>
2022-02-21 19:22:55 +0530

    Height of a binary tree

---
+++ BinaryTree/Starter_Problems/height.cpp
@@ -0,0 +1,10 @@
+// Max depth in a binary tree
+int height(BinaryTreeNode<int> *root)
+{
+    if (root == NULL)
+    {
+        return 0;
+    }
+
+    return 1 + max(height(root->left), height(root->right));
+}
