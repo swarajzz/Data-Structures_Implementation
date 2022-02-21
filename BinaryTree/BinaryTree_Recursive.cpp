4e676f200bf9b4df418b39872648974f9f4141b0
swarajzz <sharma.swaraj0403@gmail.com>
2022-02-21 08:50:37 +0530

    Recursive print & takeInput

---
+++ BinaryTree/BinaryTree_Recursive.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+#include "BinaryTreeNode_Class.h"
+using namespace std;
+
+void printTree(BinaryTreeNode<int> *root)
+{
+    if (root == NULL)
+    {
+        return;
+    }
+    cout << root->data << " : ";
+
+    if (root->left != NULL)
+    {
+        cout << "L" << root->left->data;
+    }
+
+    if (root->right != NULL)
+    {
+        cout << "R" << root->right->data;
+    }
+    cout << endl;
+
+    printTree(root->left);
+    printTree(root->right);
+}
+
+BinaryTreeNode<int> *takeInput()
+{
+    int rootData;
+    cout << "Enter data" << endl;
+    cin >> rootData;
+    if (rootData == -1)
+    {
+        return NULL;
+    }
+    BinaryTreeNode<int> *root = new BinaryTreeNode<int>(rootData);
+    BinaryTreeNode<int> *leftChild = takeInput();
+    BinaryTreeNode<int> *rightChild = takeInput();
+
+    root->left = leftChild;
+    root->right = rightChild;
+
+    return root;
+}
+
+int main()
+{
+    BinaryTreeNode<int> *root = takeInput();
+    printTree(root);
+    delete root;
+}
