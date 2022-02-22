64f7dd5935cba79e0e2eb714520122a86c6d8473
swarajzz <sharma.swaraj0403@gmail.com>
2022-02-22 19:58:15 +0530

    diameter of a binary tree

--- BinaryTree/Starter_Problems/diameter.cpp
+++ BinaryTree/Starter_Problems/diameter.cpp
@@ -1,54 +1,4 @@
-#include <iostream>
-#include "../BinaryTreeNode_Class.h"
-#include <queue>
-using namespace std;
-
-// Take input level-wise
-BinaryTreeNode<int> *takeInputLevelWise()
-{
-    int rootData;
-    cout << "Enter root's data" << endl;
-    cin >> rootData;
-    if (rootData == -1)
-    {
-        return NULL;
-    }
-
-    BinaryTreeNode<int> *root = new BinaryTreeNode<int>(rootData);
-
-    queue<BinaryTreeNode<int> *> pendingNodes;
-    pendingNodes.push(root);
-
-    while (!pendingNodes.empty())
-    {
-        BinaryTreeNode<int> *front = pendingNodes.front();
-        pendingNodes.pop();
-
-        int leftChildData;
-        cout << "Enter left child data of " << front->data << endl;
-        cin >> leftChildData;
-        if (leftChildData != -1)
-        {
-            BinaryTreeNode<int> *leftChild = new BinaryTreeNode<int>(leftChildData);
-            pendingNodes.push(leftChild);
-            front->left = leftChild;
-        }
-
-        int rightChildData;
-        cout << "Enter right child data of " << front->data << endl;
-        cin >> rightChildData;
-        if (rightChildData != -1)
-        {
-            BinaryTreeNode<int> *rightChild = new BinaryTreeNode<int>(rightChildData);
-            pendingNodes.push(rightChild);
-            front->right = rightChild;
-        }
-    }
-
-    return root;
-}
-
-int height(BinaryTreeNode<int> *root)
+/* int height(BinaryTreeNode<int> *root)
 {
 
     if (root == NULL)
@@ -58,19 +8,20 @@ int height(BinaryTreeNode<int> *root)
     return 1 + max(height(root->left), height(root->right));
 }
 
-// int diameter(BinaryTreeNode<int> *root)
-// {
-//     if (root == NULL)
-//     {
-//         return 0;
-//     }
+int diameter(BinaryTreeNode<int> *root)
+{
+    if (root == NULL)
+    {
+        return 0;
+    }
 
-//     int option1 = height(root->left) + height(root->right);
-//     int option2 = diameter(root->left);
-//     int option3 = diameter(root->right);
+    int option1 = height(root->left) + height(root->right);
+    int option2 = diameter(root->left);
+    int option3 = diameter(root->right);
 
-//     return max(option1, max(option2, option3));
-// }
+    return max(option1, max(option2, option3));
+}
+ */
 
 pair<int, int> heightDiameter(BinaryTreeNode<int> *root)
 {
@@ -93,13 +44,4 @@ pair<int, int> heightDiameter(BinaryTreeNode<int> *root)
     p.second = diameter;
 
     return p;
-}
-
-int main(){
-    BinaryTreeNode<int> * root = takeInputLevelWise();
-    pair<int, int> ans = heightDiameter(root);
-
-    cout << "Height :" << ans.first << endl;
-    cout << "Diameter :" << ans.second << endl;
-
 }
